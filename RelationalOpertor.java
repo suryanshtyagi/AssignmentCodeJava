a237b632796db31502e4f38babb055b9cdecedd5
suryanshtyagi <76549380+suryanshtyagi@users.noreply.github.com>
2022-02-02 09:27:37 +0530

    Add files via upload

---
+++ RelationalOpertor.java
@@ -0,0 +1,24 @@
+public class RelationalOpertor {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+
+        if(b<a){
+            System.out.println("a is gretert than b");
+        }
+
+        if(b<=a){
+            System.out.println("b can be equal aor less than a" );
+        }
+
+        if(b>=a){
+            System.out.println(" b can be equal or greter than a");
+        }
+
+        if(b>a){
+            System.out.println("surely b is greater than a");
+        }
+
+    }
+}

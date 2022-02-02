ece897dcf5815de1486624dad6f079c286c40e66
suryanshtyagi <76549380+suryanshtyagi@users.noreply.github.com>
2022-02-02 08:43:06 +0530

    Add files via upload

---
+++ LocalandGlobeVariable.java
@@ -0,0 +1,23 @@
+public class LocalandGlobeVariable {
+    public static void main(String[] args) {
+
+        int a ;
+        int b ; //-- Global variable
+
+
+
+    }
+    void LocalVariable(){
+
+        int a ; //--Local variable
+        int b ;
+
+
+    }
+
+
+    /**
+     * Local variable are those variables wich is declared inside the function.
+     * Globe variable are those variable wich is declared outside the function.
+     */
+}

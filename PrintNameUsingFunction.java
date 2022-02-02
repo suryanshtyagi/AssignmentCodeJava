585287642736e384c4671cdf36a335f3eddeeb24
suryanshtyagi <76549380+suryanshtyagi@users.noreply.github.com>
2022-02-02 08:42:12 +0530

    Add files via upload

---
+++ PrintNameUsingFunction.java
@@ -0,0 +1,12 @@
+public class PrintNameUsingFunction {
+    public static void main(String[] args) {
+        PrintNameUsingFunction name = new PrintNameUsingFunction();
+        System.out.println(name.printName());
+
+
+    }
+    String printName(){
+        String name = "Suryansh Tyagi";
+        return name;
+    }
+}

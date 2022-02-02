ae76083169429e9708cea452f0c60713b99a85a7
suryanshtyagi <76549380+suryanshtyagi@users.noreply.github.com>
2022-02-02 08:40:09 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,22 @@
+public class Comments {
+    public static void main(String[] args) {
+        //-- This is single line comment
+
+
+        /*
+        This
+        is
+        multiline
+        comment
+         */
+
+        /**
+         * This
+         * is
+         * docummantion
+         * comment
+         */
+
+
+    }
+}

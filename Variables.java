a70ae89ecc4435fe33aa1b44e279b3549542543d
suryanshtyagi <76549380+suryanshtyagi@users.noreply.github.com>
2022-02-02 08:41:23 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int number = 10;
+        float percentage = 88.4f;
+        char c = 's';
+        boolean k =false;
+        double big = 10.676847598479;
+        System.out.println(number);
+        System.out.println(percentage);
+        System.out.println(c);
+        System.out.println(k);
+        System.out.println(big);
+
+    }
+}

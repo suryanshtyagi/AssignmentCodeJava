b1c871433a2318280282fd9ef8db8c2ee6e42cc2
suryanshtyagi <76549380+suryanshtyagi@users.noreply.github.com>
2022-02-02 09:25:15 +0530

    Add files via upload

---
+++ FunctionForArthMeticOprtrs.java
@@ -0,0 +1,14 @@
+public class FunctionForArthMeticOprtrs {
+    public static void main(String[] args) {
+
+    }
+
+    void AirthmaticOpertaors(){
+        int a  =100;
+        int b = 200;
+        int sum  = a+b; // here "+" is used for add  numbers.
+        int subtract = a-b; // here "-" is used for subtraction.
+        int multiply = a*b; //here "*" is used for multiplication.
+        int divide = a/b; // here "/" is used for divide numbers.
+    }
+}
